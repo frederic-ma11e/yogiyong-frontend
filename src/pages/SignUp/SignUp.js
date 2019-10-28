3fc7c79427e10e7ac593ae4cb03c6af0a0bd1290
률루랄라 <doryannim@Chungui-MacBook-Pro.local>
2019-10-28 17:25:11 +0900

    test

---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.scss';
+
+function SignUp() {
+    return (
+        <div>
+            SignUp Page
+        </div>
+    );
+}
+
+export default SignUp;
