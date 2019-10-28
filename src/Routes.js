c83e023ab11259d21b4b8487bfc973798bfd5241
Jisu <dbswltn111@gmail.com>
2019-10-28 17:13:46 +0900

    JisuTest_commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "../src/pages/Main/index.js";
+import Login from "../src/pages/Login/index.js"
 
 class Routes extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </Router>
     );
