0ca16c6900a7c5a1868bdbcf7e91a7f56298544f
률루랄라 <doryannim@Chungui-MacBook-Pro.local>
2019-11-01 14:29:35 +0900

    storelist 백업

--- src/Routes.js
+++ src/Routes.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "../src/pages/Main/index.js";
 import Login from "../src/pages/Login/index.js";
-import SignUp from './pages/SignUp';
+import SignUp from './pages/SignUp/index.js';
 import RestaurantInfo from "./pages/RestautantInfo/index.js";
+import StoreList from "./pages/StoreList/index.js"
 
 class Routes extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class Routes extends React.Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/Login/SignUp" component={SignUp} />
           <Route exact path="/detailMenu" component={RestaurantInfo} />
+          <Route exact path="/storelist" component={StoreList} />
         </Switch>
       </Router>
     );
