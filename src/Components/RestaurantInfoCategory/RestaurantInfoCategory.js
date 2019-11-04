2ae2b4c865a14755966e592da79ab7f55c824302
AddChan <is528629@gmail.com>
2019-11-04 14:58:38 +0900

    restaurantInfoCatrgory API 작업 완료

--- src/Components/RestaurantInfoCategory/RestaurantInfoCategory.js
+++ src/Components/RestaurantInfoCategory/RestaurantInfoCategory.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import "./RestaurantInfoCategory.scss";
 import RestaurantInfoCList from "./RestaurantInfoCList";
 class RestaurantInfoCategory extends Component {
+
+
   render() {
-    const { data } = this.props;
+    const { data } = this.props ;
     const list = data.map((info, index) => (
       <RestaurantInfoCList key={index} info={info} />
     ));
