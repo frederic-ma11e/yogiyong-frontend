0360437825267a59c63ce762f372c9896750edf9
saegeullee <lemix777@naver.com>
2019-11-07 21:01:29 +0900

    Implemented order list page, added header and footer to thankyou and order list page

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import foodOrder from './pages/FoodOrderPage';
 import StoreList from "./pages/StoreList/index.js"
 import GoogleMaps from "./Components/GoogleMaps"
 import OrderThankyou from "./pages/OrderThankyou/"
+import OrderedList from "./pages/OrderedList"
 
 class Routes extends React.Component {
   render() {
@@ -23,6 +24,7 @@ class Routes extends React.Component {
           <Route exact path="/storelist" component={StoreList} />
           <Route exact path="/google-maps" component={GoogleMaps} />
           <Route exact path="/thank-you" component={OrderThankyou} />
+          <Route exact path="/ordered-list" component={OrderedList} />
         </Switch>
       </Router>
     );
