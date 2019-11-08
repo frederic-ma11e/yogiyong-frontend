4eda1333e244a320e43f552c8b9f0e2b92ab2d46
saegeullee <lemix777@naver.com>
2019-11-08 15:53:06 +0900

    Updated each orderd menus total price

--- src/Components/FoodOrderPage/FoodOrderList/OrderMenu.js
+++ src/Components/FoodOrderPage/FoodOrderList/OrderMenu.js
@@ -4,7 +4,7 @@ import OrderMenuList from "./OrderMenuList";
 class OrderMenu extends Component {
   render() {
     const {data} = this.props
-    const list = data.map((data, index) => (
+    const list = data.map((data, index) => ( 
       <OrderMenuList key={index} info={data} />
     ));
     return <div>{list}</div>;
--- src/Components/FoodOrderPage/FoodOrderList/OrderMenuList.js
+++ src/Components/FoodOrderPage/FoodOrderList/OrderMenuList.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class OrderMenuList extends Component {
   render() {
-    const { name, count, price } = this.props.info
+    const { name, count, price, total_price} = this.props.info
     return (
       <li className="list-group-item">
         <div className="menu-title">
           {name} x {count}개
         </div>
-        <div className="menu-price">{price}원</div>
+        <div className="menu-price">{total_price}원</div>
       </li>
     );
   }
